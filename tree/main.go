85820d2cd636fd83c5523664923a1000729c1c49
cuongnm6 <cuongnm6@vng.com.vn>
2024-10-10 10:14:25 +0700

    fix remove func

--- tree/main.go
+++ tree/main.go
@@ -9,7 +9,7 @@ import (
 // Binary tree
 type Node[T constraints.Ordered] struct {
 	value T
-	left *Node[T]
+	left  *Node[T]
 	right *Node[T]
 }
 
@@ -17,16 +17,15 @@ type BinaryTree[T constraints.Ordered] struct {
 	root *Node[T]
 }
 
-
 func (b *BinaryTree[T]) insert(value T) {
 	b.insertRec(b.root, value)
 }
 
 func (b *BinaryTree[T]) insertRec(node *Node[T], value T) *Node[T] {
 	if b.root == nil {
-        b.root = &Node[T]{value: value, right: nil, left: nil}
-        return b.root
-    }
+		b.root = &Node[T]{value: value, right: nil, left: nil}
+		return b.root
+	}
 
 	if node == nil {
 		node = &Node[T]{value: value, right: nil, left: nil}
@@ -35,20 +34,20 @@ func (b *BinaryTree[T]) insertRec(node *Node[T], value T) *Node[T] {
 
 	if node.value > value {
 		node.left = b.insertRec(node.left, value)
-	}else {
+	} else {
 		node.right = b.insertRec(node.right, value)
 	}
 	return node
 }
 
-func (b *BinaryTree[T]) travelLeftToRootToRight(node *Node[T]){
-	if(b == nil){
+func (b *BinaryTree[T]) travelLeftToRootToRight(node *Node[T]) {
+	if b == nil {
 		return
 	}
 	if node.right == nil && node.left == nil {
 		fmt.Println(node.value)
 		return
-	}else {
+	} else {
 		b.travelLeftToRootToRight(node.left)
 		fmt.Println(node.value)
 		b.travelLeftToRootToRight(node.right)
@@ -56,13 +55,13 @@ func (b *BinaryTree[T]) travelLeftToRootToRight(node *Node[T]){
 }
 
 func (b *BinaryTree[T]) travelLeftToRightToRoot(node *Node[T]) {
-	if(b == nil){
+	if b == nil {
 		return
 	}
 	if node.right == nil && node.left == nil {
 		fmt.Println(node.value)
 		return
-	}else {
+	} else {
 		b.travelLeftToRightToRoot(node.left)
 		b.travelLeftToRightToRoot(node.right)
 		fmt.Println(node.value)
@@ -70,39 +69,39 @@ func (b *BinaryTree[T]) travelLeftToRightToRoot(node *Node[T]) {
 }
 
 func (b *BinaryTree[T]) travelRootToLeftToRight(node *Node[T]) {
-	if(b == nil){
+	if b == nil {
 		return
 	}
 	if node.right == nil && node.left == nil {
 		fmt.Println(node.value)
 		return
-	}else {
+	} else {
 		fmt.Println(node.value)
 		b.travelRootToLeftToRight(node.left)
 		b.travelRootToLeftToRight(node.right)
 	}
 }
 
-func (b *BinaryTree[T]) travelByLevel(){
-	if(b == nil){
+func (b *BinaryTree[T]) travelByLevel() {
+	if b == nil {
 		return
 	}
-	queue:= []*Node[T]{b.root}
+	queue := []*Node[T]{b.root}
 
 	for len(queue) > 0 {
-		last := queue[len(queue) - 1];
+		last := queue[len(queue)-1]
 		fmt.Println(last.value)
-		queue = queue[:len(queue) - 1]
-		if(last.left != nil){
+		queue = queue[:len(queue)-1]
+		if last.left != nil {
 			queue = append([]*Node[T]{last.left}, queue...)
 		}
-		if(last.right != nil){
+		if last.right != nil {
 			queue = append([]*Node[T]{last.right}, queue...)
 		}
 	}
 }
 
-func (b *BinaryTree[T]) find(node *Node[T], value T) (*Node[T], error){
+func (b *BinaryTree[T]) find(node *Node[T], value T) (*Node[T], error) {
 	if b == nil {
 		var zeroVal *Node[T]
 		return zeroVal, fmt.Errorf("empty tree")
@@ -113,7 +112,7 @@ func (b *BinaryTree[T]) find(node *Node[T], value T) (*Node[T], error){
 		fmt.Println("Not found!!!")
 		return zeroVal, fmt.Errorf("not found")
 	}
-	
+
 	if node.value == value {
 		fmt.Println("Found!!!")
 		return node, nil
@@ -127,36 +126,33 @@ func (b *BinaryTree[T]) find(node *Node[T], value T) (*Node[T], error){
 
 }
 
-func (b *BinaryTree[T]) remove(node *Node[T], value T){
+func (b *BinaryTree[T]) remove(node *Node[T], value T) *Node[T] {
 	if node == nil {
-		fmt.Println("Nothing to remove")
-		return
+		return nil
 	}
 
 	if node.value > value {
-		b.remove(node.left, value)
-	}else if node.value < value {
-		b.remove(node.right, value)
-	}else {
+		node.left = b.remove(node.left, value)
+		return node
+	} else if node.value < value {
+		node.right = b.remove(node.right, value)
+		return node
+	} else {
 		if node.left == nil && node.right == nil {
-            node = nil
-        } else if node.left == nil {
-            node = node.right
-        } else if node.right == nil {
-            node = node.left
-        } else {
-            minNode, err := b.minNode(node)
-            if err!= nil {
-                fmt.Println("Error finding min node")
-                return
-            }
-            node.value = minNode.value
-            b.remove(minNode, value)
-        }
-        return
+			node = nil
+			return node
+		} else if node.left == nil {
+			node = node.right
+		} else if node.right == nil {
+			node = node.left
+		} else {
+			minNode, _ := b.minNode(node)
+			node.value = minNode.value
+			node.right = b.remove(minNode, minNode.value)
+		}
+		return node
 	}
-	
-	
+
 }
 
 func (b *BinaryTree[T]) minNode(node *Node[T]) (*Node[T], error) {
@@ -166,11 +162,10 @@ func (b *BinaryTree[T]) minNode(node *Node[T]) (*Node[T], error) {
 
 	node = node.right
 	for node.left != nil {
-		node= node.left
+		node = node.left
 	}
 	return node, nil
 }
-
 
 func main() {
 	tree := &BinaryTree[int]{}
@@ -179,15 +174,16 @@ func main() {
 	tree.insert(12)
 	tree.insert(6)
 	tree.insert(9)
-	// fmt.Println(tree.root.value) // 10
-	// fmt.Println(tree.root.left.value) // 8
-	// fmt.Println(tree.root.right.value) // 11
-	// fmt.Println(tree.root.left.right.value) // 9
+
 	// tree.travelLeftToRootToRight(tree.root)
 	// tree.travelLeftToRightToRoot(tree.root)
 	// tree.travelRootToLeftToRight(tree.root)
 	// tree.travelByLevel()
 	// tree.find(tree.root, 9)
-	tree.remove(tree.root, 8)
-	fmt.Println(tree.root.left.value)//9
+	tree.remove(tree.root, 10)
+	fmt.Println(tree.root.value)           // 10
+	fmt.Println(tree.root.left.value)      // 8
+	fmt.Println(tree.root.right)     // 12
+	fmt.Println(tree.root.left.right.value)      // 9
+	fmt.Println(tree.root.left.left.value) // 9
 }
