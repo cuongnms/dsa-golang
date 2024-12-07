1dfe0155a0b765bf31d0d14e948480e9fbb0a2e9
CuongNMs <nguyen.minh.cuong.1102@gmail.com>
2024-12-07 23:01:38 +0700

    add dfs graph

--- leetcode75/graph-dfs.go
+++ leetcode75/graph-dfs.go
@@ -37,11 +37,11 @@ All the values of rooms[i] are unique.
 */
 
 func canVisitAllRooms(rooms [][]int) bool {
-    lenRoom := len(rooms)
+	lenRoom := len(rooms)
 	visited := make([]bool, lenRoom)
 	visited[0] = true
-	var dfs func(roomIndex int)  
-	dfs = func(roomIndex int)  {
+	var dfs func(roomIndex int)
+	dfs = func(roomIndex int) {
 		fmt.Println("roomIndex: ", roomIndex, " value: ", rooms[roomIndex])
 		for _, indexOtherRoom := range rooms[roomIndex] {
 			if visited[indexOtherRoom] {
@@ -49,9 +49,9 @@ func canVisitAllRooms(rooms [][]int) bool {
 			}
 			visited[indexOtherRoom] = true
 			if isVisitedAll(visited) {
-				return 
-			}else {
-                 dfs(indexOtherRoom)
+				return
+			} else {
+				dfs(indexOtherRoom)
 			}
 		}
 	}
@@ -60,16 +60,66 @@ func canVisitAllRooms(rooms [][]int) bool {
 
 	if isVisitedAll(visited) {
 		return true
-	}else {
+	} else {
 		return false
 	}
 }
 
 func isVisitedAll(visited []bool) bool {
 	for _, v := range visited {
-        if !v {
-            return false
-        }
-    }
-    return true
+		if !v {
+			return false
+		}
+	}
+	return true
+}
+
+/*
+There are n cities. Some of them are connected, while some are not.
+If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+Return the total number of provinces.
+
+Example 1:
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2
+
+Example 2:
+Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+
+Constraints:
+1 <= n <= 200
+n == isConnected.length
+n == isConnected[i].length
+isConnected[i][j] is 1 or 0.
+isConnected[i][i] == 1
+isConnected[i][j] == isConnected[j][i]
+*/
+
+func findCircleNum(isConnected [][]int) int {
+	lenInput := len(isConnected)
+	isChecked := make([]bool, lenInput)
+	var dfs func(currentCityIndex int)
+	count := 0
+	dfs = func(currentCityIndex int) {
+		isChecked[currentCityIndex] = true
+		for index, city := range isConnected[currentCityIndex] {
+			if city == 1 && index != currentCityIndex && !isChecked[index] {
+				dfs(index)
+			}
+		}
+		
+	}
+
+	for i:=0; i < len(isConnected[0]); i++ {
+		if !isChecked[i] {
+			count++
+			dfs(i)
+		}
+	}
+	
+	return count
+
 }
