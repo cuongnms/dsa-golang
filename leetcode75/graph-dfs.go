32f185826d7ee5d8672a7df7e7793570fb8bdd48
CuongNMs <nguyen.minh.cuong.1102@gmail.com>
2024-12-08 23:37:19 +0700

    add dfs graph

--- leetcode75/graph-dfs.go
+++ leetcode75/graph-dfs.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 // import "fmt"
 
@@ -110,16 +113,77 @@ func findCircleNum(isConnected [][]int) int {
 				dfs(index)
 			}
 		}
-		
+
 	}
 
-	for i:=0; i < len(isConnected[0]); i++ {
+	for i := 0; i < len(isConnected[0]); i++ {
 		if !isChecked[i] {
 			count++
 			dfs(i)
 		}
 	}
-	
+
 	return count
+
+}
+
+/*
+There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between two different cities (this network form a tree).
+Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
+This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
+It's guaranteed that each city can reach city 0 after reorder.
+
+Example 1:
+Input: n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+Output: 3
+Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+
+Example 2:
+Input: n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
+Output: 2
+Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+Example 3:
+
+Input: n = 3, connections = [[1,0],[2,0]]
+Output: 0
+
+Constraints:
+
+2 <= n <= 5 * 104
+connections.length == n - 1
+connections[i].length == 2
+0 <= ai, bi <= n - 1
+ai != bi
+*/
+
+func minReorder(n int, connections [][]int) int {
+	var dfs func(int)
+	count := 0
+	isPathVisit := make([]bool, len(connections))
+	dfs = func(city int) {
+		for index, val := range connections {
+			if isPathVisit[index] {
+				continue
+			}
+			if slices.Contains(val, city) {
+				isPathVisit[index] = true
+				if val[0] == city {
+					count++
+					dfs(val[1])
+				} else {
+					dfs(val[0])
+				}
+			}
+		}
+	}
+
+	dfs(0)
+
+	return count
+
+
+
 
 }
