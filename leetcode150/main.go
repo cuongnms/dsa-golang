2b7dfa1bc187cac855fb0288ac819bc239a87a21
cuongnm6 <cuongnm6@vng.com.vn>
2025-02-14 13:22:25 +0700

    add kadane quiz

--- leetcode150/main.go
+++ leetcode150/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	//============150==============================
-	root:= sortedArrayToBST([]int{-10, -3, 0, 5, 9})
-	fmt.Println(root)
+	// root:= sortedArrayToBST([]int{-10, -3, 0, 5, 9})
+	// fmt.Println(root)
+	fmt.Println(maxSubArray([]int{-2, 1}))
 }
