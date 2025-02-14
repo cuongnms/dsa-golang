f5bbf31c67af57a76e887915768167b1bf58a036
CuongNMs <nguyen.minh.cuong.1102@gmail.com>
2025-02-15 00:23:14 +0700

    add kadane quiz

--- leetcode150/kadane.go
+++ leetcode150/kadane.go
@@ -42,3 +42,72 @@ func maxSubArray(nums []int) int {
 	}
 	return max
 }
+
+
+/*
+Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+
+A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
+
+A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+
+Example 1:
+
+Input: nums = [1,-2,3,-2]
+Output: 3
+Explanation: Subarray [3] has maximum sum 3.
+Example 2:
+
+Input: nums = [5,-3,5]
+Output: 10
+Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10.
+Example 3:
+
+Input: nums = [-3,-2,-3]
+Output: -2
+Explanation: Subarray [-2] has maximum sum -2.
+ 
+Constraints:
+
+n == nums.length
+1 <= n <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+*/
+func maxSubarraySumCircular(nums []int) int {
+	sumArr:=nums[0]
+	minSum:=nums[0]
+	maxSum:=nums[0]
+	currentMin:=nums[0]
+	currentMax:=nums[0]
+
+	for i:=1; i < len(nums); i++ {
+		sumArr += nums[i]
+		if currentMax + nums[i] > nums[i] {
+			currentMax += nums[i]
+		}else {
+			currentMax = nums[i]
+		}
+		if maxSum < currentMax {
+			maxSum = currentMax
+		}
+
+		if currentMin + nums[i] < nums[i] {
+            currentMin += nums[i]
+        }else {
+            currentMin = nums[i]
+        }
+		if minSum > currentMin {
+            minSum = currentMin
+        }
+	}
+	if maxSum <= 0 {
+		return maxSum
+	}else {
+		if maxSum > sumArr - minSum {
+			return maxSum
+		}else {
+			return sumArr - minSum
+		}
+	}
+}
+
