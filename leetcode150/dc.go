cd1ce7b64279bba85f96729b290f873b8c329655
cuongnm6 <cuongnm6@vng.com.vn>
2025-03-17 13:47:51 +0700

    add divide and conquer quiz

--- leetcode150/dc.go
+++ leetcode150/dc.go
@@ -48,3 +48,84 @@ func sortedArrayToBST(nums []int) *TreeNode {
 	return recursive(nums, 0, len(nums)-1)
 }
 
+/*
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Example 1:
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+ 
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+*/
+func sortList(head *ListNode) *ListNode {
+
+	var merge func(l1, l2 *ListNode) *ListNode
+
+	merge = func(l1, l2 *ListNode) *ListNode {
+		tmp := &ListNode{Val: 0, Next: nil}
+		moving:= tmp
+		for l1 != nil && l2 != nil {
+			if l1.Val < l2.Val {
+				moving.Next = l1
+				l1 = l1.Next
+			}else {
+				moving.Next = l2
+				l2 = l2.Next
+			}
+			moving = moving.Next
+		}
+
+		if l1 == nil {
+			moving.Next = l2
+		}
+
+		if l2 == nil {
+			moving.Next = l1
+		}
+
+		return tmp.Next
+
+	}
+
+	var sort func(head *ListNode) *ListNode
+
+
+	sort = func(head *ListNode) *ListNode{
+		if head == nil || head.Next == nil {
+			return head
+		}
+
+		current:= head
+		mid:=head
+		var tmp *ListNode
+		for current != nil && current.Next != nil{
+			tmp = mid
+			mid = mid.Next
+			current = current.Next.Next
+		}
+		tmp.Next = nil
+
+		l1:= sort(head)
+		l2:= sort(mid)
+		return merge(l1,l2)
+
+	}
+
+	return sort(head)
+	
+
+}
--- leetcode150/main.go
+++ leetcode150/main.go
@@ -13,13 +13,19 @@ func main() {
 	// }
 	// root := CreateTree(values)
 	// fmt.Println(kthSmallest(root, 1))
-	// values:= []interface{}{1,2}
-	// list:= CreateLL(values)
+	values:= []interface{}{4,2,1,3}
+	list:= CreateLL(values)
 	// fmt.Println(rotateRight(list, 1))
 	// fmt.Println(permute([]int{1,2,3}))
 	// fmt.Println(combinationSum([]int{8,7,4,3},11))
-	fmt.Println(exist([][]byte{{'A', 'B', 'C', 'E'},
-	{'S', 'F', 'C', 'S'},
-	{'A', 'D', 'E', 'E'}}, "ABCCED"))
-	fmt.Println(generateParenthesis(3))
+	// fmt.Println(exist([][]byte{{'A', 'B', 'C', 'E'},
+	// {'S', 'F', 'C', 'S'},
+	// {'A', 'D', 'E', 'E'}}, "ABCCED"))
+	// fmt.Println(generateParenthesis(3))
+	tmp := sortList(list)
+	for(tmp != nil) {
+		fmt.Println(tmp.Val)
+		tmp = tmp.Next
+	}
+	
 }
